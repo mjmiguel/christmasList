ebfcfe18a2bf0a5caa9150c04fddff3429a81231
mjmiguel <migueljmiller@gmail.com>
2020-09-26 15:45:10 -0700

    add onchange; start sesh auth

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,12 +1,25 @@
-/* eslint-disable arrow-body-style */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = (props) => {
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div>
       <label htmlFor="password">Password:</label>
-      <input type="password" id="login-password" name="login-password" />
+      <input
+        value={password}
+        type="password"
+        id="login-password"
+        name="login-password"
+        onChange={(e) => { handleChange(e); }}
+      />
     </div>
   );
 };
+
+export default Login;
--- client/index.js
+++ client/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Login from './components/Login';
+
+// TODO: add session auth
+const authenticated = false;
 
 render(
   <BrowserRouter>
-    <App />
+    {authenticated ? <App /> : <Login />}
   </BrowserRouter>,
   document.getElementById('root'),
 );
