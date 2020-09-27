cd8e016da0ea7aaa4b05cb910e77a95250e08fbe
mjmiguel <migueljmiller@gmail.com>
2020-09-27 10:25:08 -0700

    add hooks and button

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,23 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import fetch from 'node-fetch';
 
 const Login = (props) => {
   const [password, setPassword] = useState('');
+  const [validation, setValidation] = useState(false);
+  const [lenValidation, setLenValidation] = useState(true);
+
+  useEffect(() => {
+
+  }, [lenValidation]);
 
   const handleChange = (e) => {
     setPassword(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submit', e.target.value);
+    if (process.env !== 'test') {
+      if (password.length > 0) {
+        setLenValidation(true);
+        // send fetch
+        // set validation
+      } else {
+        // render some text validation
+        setLenValidation(false);
+      }
+    }
+  };
+
   return (
     <div>
-      <label htmlFor="password">Password:</label>
-      <input
-        value={password}
-        type="password"
-        id="login-password"
-        name="login-password"
-        onChange={(e) => { handleChange(e); }}
-      />
+      <form action={handleSubmit}>
+        <label htmlFor="password">Password:</label>
+        <input
+          value={password}
+          type="password"
+          id="login-password"
+          name="login-password"
+          onChange={(e) => { handleChange(e); }}
+        />
+        {lenValidation ? null : <div>probably not a valid password</div>}
+        <input 
+          type="submit"
+          value="Submit"
+        />
+      </form>
     </div>
   );
 };
