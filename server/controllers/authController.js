0289b05821b00074c35b71ee19f5c2cdda8d649c
mjmiguel <migueljmiller@gmail.com>
2020-11-20 18:27:21 -0800

    add update password function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,34 +1,40 @@
 const db = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const SALT_FACTOR = 12; // should be at least 10
+require('dotenv').config();
 
 const authController = {};
+const { SITE_LOGIN_PASSWORD } = process.env;
 
-//  get a specific person from req.params
-// userController.getUser = (req, res, next) => {
-//   const oneUser = req.query.id;
-//   // using parameterized query
-//   const queryString = 'SELECT * FROM users WHERE user_id=$1';
-//   const params = [oneUser];
-//   db.query(queryString, params)
-//     .then((users) => {
-//       res.locals.users = users.rows;
-//       next();
-//     }).catch((err) => next(err));
-// };
+//  update passwords for ALL users
+authController.updatePassword = (req, res, next) => {
+  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+    bcrypt.hash(SITE_LOGIN_PASSWORD, salt, function (err, hash) {
+      // Store hash in DB.
+      const queryString = 'UPDATE users SET password = $1';
+      const params = [hash];
+      db.query(queryString, params)
+        .then((res) => {
+          console.log('passwords updated');
+          next();
+        })
+        .catch((err) => next({ location: 'updatePassword query', ...err }));
+    });
+  });
+};
 
 authController.checkLogin = (req, res, next) => {
-  // boilerplate for now to rest middleware
   const { password } = req.body;
   const queryString = 'SELECT * FROM users WHERE id=1';
-  // const params = [oneUser];
   console.log('req body', req.body);
   db.query(queryString)
     .then((users) => {
-      console.log('users', users);
+      console.log('users', users.rows[0]);
       res.locals.users = users.rows[0];
       bcrypt
         .compare(password, res.locals.users.password)
         .then((res) => {
+          console.log('res', res);
           res ? (res.locals.authorized = true) : (res.locals.authorized = false);
           next();
         })
