b9122f8f2f2ac93fe3f50dc90f18c94a682a6901
mjmiguel <migueljmiller@gmail.com>
2020-11-25 18:31:50 -0800

    add spinner render if checking tokens

--- client/components/PrivateRoute.jsx
+++ client/components/PrivateRoute.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useAuth } from '../context/auth';
+import bubs from '../assets/bubs.png';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { authTokens } = useAuth();
+  const { authTokens, tokenVerified, setTokenVerified, verifyTokens } = useAuth();
+
+  // check token each time the component rendered changes
+  useEffect(() => {
+    if ((authTokens && tokenVerified === false) || (authTokens && tokenVerified === 'loading')) {
+      verifyTokens(authTokens);
+    }
+  }, [Component]);
+
+  // if no auth tokens (initial load), change tokenverified to false
+  useEffect(() => {
+    if (!authTokens) setTokenVerified(false);
+  }, [tokenVerified]);
 
   return (
     <Route
       {...rest}
-      render={(props) => (authTokens ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: '/login', state: { referer: props.location } }} />
-      ))}
+      render={(props) => {
+        switch (tokenVerified) {
+          case true:
+            return <Component {...props} />;
+          case false:
+            return <Redirect to={{ pathname: '/login', state: { referer: props.location } }} />;
+          case 'loading':
+            return (
+              <div>
+                <img className="loading" src={bubs} alt="bubs" height="200px" width="200px" />
+              </div>
+            );
+          default:
+            console.error('something wrong in privateROute switch');
+        }
+      }}
     />
   );
 };
