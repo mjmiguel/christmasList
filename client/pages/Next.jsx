7703e32ad44f6d620693a98f929b590373ee95cd
mjmiguel <migueljmiller@gmail.com>
2020-11-29 21:24:31 -0800

    add navigation

--- client/pages/Next.jsx
+++ client/pages/Next.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable arrow-body-style */
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import dog from '../assets/next.jpg';
 
-const Next = props => {
+const Next = (props) => {
   return (
     <div className="router">
       <main>
         <img alt="dog" src={dog} height="300" width="500" />
         <h3>your list has been submitted :)</h3>
+        <section style={{ display: 'inline' }}>
+          <Link to="/">
+            <button type="button">Home</button>
+          </Link>
+
+          <Link to="/submit">
+            <button type="button">Try Again</button>
+          </Link>
+        </section>
       </main>
     </div>
   );
